0909e596efe219d219ed0708152556437d60709d
EddyViet <eddyviettc@gmail.com>
2023-05-31 12:43:52 +0700

    Building sidebar UI #1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,9 +7,11 @@ import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
 const publicRoutes = [
     { patch: config.routes.home, component: Home },
     { patch: config.routes.following, component: Following },
+    { patch: config.routes.live, component: Live },
     { patch: config.routes.profile, component: Profile },
     { patch: config.routes.upload, component: Upload, layout: HeaderOnly },
     { patch: config.routes.search, component: Search, layout: null },
